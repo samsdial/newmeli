01769ff184ca5a89f3fbeb5b283041947bac0c2c
Sergio Andres Martinez E <sergio.martinez@fracttal.com>
2025-07-28 23:32:31 -0500

    FIXED: Error 400 search in production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,4 @@
 export const getApiUrl = (endpoint) => {
-  // Detectar si estamos en desarrollo por la presencia del puerto 3000
   const isDevelopment =
     window.location.hostname === "localhost" ||
     window.location.hostname === "127.0.0.1";
